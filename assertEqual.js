487563dfc9bde97edef6966524fc27f68514086b
Sandra To <sandra.to.e@gmail.com>
2021-02-02 03:28:33 +0000

    Created assertion function with emoji

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,16 @@
 //FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 //TEST CODE
 assertEqual('Lighthouse Labs', 'Bootcamp');
 assertEqual(1, 1);
+assertEqual(3, 4);
+assertEqual(true, true);
+assertEqual('45', 45);
+assertEqual('45', '45');
