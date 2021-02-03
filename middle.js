b91aad1d9194bf023ae7a5163c3278acff3bbca8
Sandra To <sandra.to.e@gmail.com>
2021-02-03 20:39:01 +0000

    Assertion functions and Test codes added

---
+++ middle.js
@@ -0,0 +1,42 @@
+// TEST ASSERTION FUNCTIONS
+
+// eqArrays function
+const eqArrays = (arrOne, arrTwo) => {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// assertion function for arrays
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+// CHALLENGE
+// Create `middle` function to return only the middlemost element in an array
+// If array is 1 or 2 elements, no middle, return empty array
+// If array is odd # elements, return single middle element
+// If array is even # elements, return two middle elements
+
+// ACTUAL MIDDLE FUNCTION
+
+
+// TEST CODES
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2], []));
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
