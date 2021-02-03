1150ea87428b5cb6d0a2646cc6a84e8c9e8c0859
Sandra To <sandra.to.e@gmail.com>
2021-02-03 07:09:20 +0000

    Noted out assertEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,11 @@
-// assertion function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// // assertion function
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 // eqArrays function
 const eqArrays = (arrOne, arrTwo) => {
