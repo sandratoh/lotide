203e98ce656a843052edadab6269311f0c257800
Sandra To <sandra.to.e@gmail.com>
2021-02-03 21:36:36 +0000

    Middle function added

--- middle.js
+++ middle.js
@@ -29,28 +29,43 @@ const assertArraysEqual = function(arrOne, arrTwo) {
 // If array is even # elements, return two middle elements
 
 // PSUEDOCODE
-Define middle function (arr)
-  new variable emptyArr = [];
-  middleIndex = 
-  if arr.length with 1 or 2 (can use 0 or less than 3 elements OR === 1 || === 2)
-    return emptyArr as [];
-  if arr.length % 2 !== 0 
-    find middle element index
-    middleIndex = (arr.length - 1)/2
-    return emptyArr = arr[middleIndex]
-  if arr.length % 2 === 0
-    find middle two elements
-    middleIndex[1] = (arr[arr.length / 2])
-    middleIndex[0] = (arr[arr.length / 2 - 1])
-    return emptyArr = arr[middleIndex];
-End function
+// Define middle function (arr)
+//   new variable emptyArr = [];
+//   middleIndex =
+//   if arr.length with 1 or 2 (can use 0 or less than 3 elements OR === 1 || === 2)
+//     return emptyArr as [];
+//   if arr.length % 2 !== 0
+//     find middle element index
+//     middleIndex = (arr.length - 1)/2
+//     return emptyArr = arr[middleIndex]
+//   if arr.length % 2 === 0
+//     find middle two elements
+//     middleIndex[1] = (arr[arr.length / 2])
+//     middleIndex[0] = (arr[arr.length / 2 - 1])
+//     return emptyArr = arr[middleIndex];
+// End function
 
 // ACTUAL MIDDLE FUNCTION
+// Can add in variable for middleElementIndex for clarification
+const middle = (arr) => {
+  let middleElements = [];
+  if (arr.length <= 2) {
+    return middleElements;
+  }
+  if (arr.length % 2 !== 0) {
+    middleElements.push(arr[(arr.length - 1) / 2]);
+  }
+  if (arr.length % 2 === 0) {
+    middleElements[1] = (arr[arr.length / 2]);
+    middleElements[0] = (arr[arr.length / 2 - 1]);
+  }
+  return middleElements;
+};
 
 
 // TEST CODES
 assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2], []));
+assertArraysEqual(middle([1, 2]), []);
 
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
