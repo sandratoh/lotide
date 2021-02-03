3dfac87990802e78a90536f49cc1b4b13b3d2f85
Sandra To <sandra.to.e@gmail.com>
2021-02-03 22:48:37 +0000

    Assertion function added

---
+++ countOnly.js
@@ -0,0 +1,8 @@
+// ASSERTION FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
