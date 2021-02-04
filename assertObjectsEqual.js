aab5e06367e7458c6f7ea82e01f7b708e7e2dcd9
Sandra To <sandra.to.e@gmail.com>
2021-02-04 08:54:56 +0000

    Assert objects function added

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -15,3 +15,22 @@ const eqObjects = function(object1, object2) {
   }
   return true;
 };
+
+// Function behaviour:
+// Input: take in two objects
+// Output: console.log appropriate message passing or failing
+// Use util library's inspect function to see detailed object output
+
+const assertObjectsEqual = (actualObj, expectedObj) => {
+  // import util library to use inspect function
+  const inspect = require('util').inspect;
+  if (eqObjects(actualObj, expectedObj)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actualObj)} === ${inspect(expectedObj)}`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed: ${inspect(actualObj)} !== ${inspect(expectedObj)}`);
+  }
+};
+
+// Test cases:
+assertObjectsEqual({ a : 1 }, { a : 1});
+assertObjectsEqual({ a : 1 }, { a : '1'});
