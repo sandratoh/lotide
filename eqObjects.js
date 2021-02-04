a733c8f48f34c4cc9dbb7aba827c840698bf0293
Sandra To <sandra.to.e@gmail.com>
2021-02-04 04:56:30 +0000

    Assertion functions and eqObjects start function added

---
+++ eqObjects.js
@@ -0,0 +1,13 @@
+// ASSERTION FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};
