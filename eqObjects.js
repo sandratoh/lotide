a8de860b253cb75c086a2ca0260b2bea635666e1
Sandra To <sandra.to.e@gmail.com>
2021-02-04 05:00:44 +0000

    Test codes and notes added

--- eqObjects.js
+++ eqObjects.js
@@ -7,7 +7,22 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// CHALLENGE
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
+
+// ACTUAL FUNCTION
 const eqObjects = function(object1, object2) {
 };
+
+
+// TEST CODES
+const ab = { a: '1', b: '2'};
+const ba = { b: '2', a: '1'};
+eqObjects(ab, ba); // => true
+
+const abc = { a: '1', b: '2', c: '3'};
+eqObjects(ab, abc); // => false
+
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
