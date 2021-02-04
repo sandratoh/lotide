cb554cfdc6a680d2a1dfb657e7b9680c3d6e7dd0
Sandra To <sandra.to.e@gmail.com>
2021-02-04 04:47:36 +0000

    Function added and passed all assertions

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -13,6 +13,17 @@ const assertEqual = function(actual, expected) {
 // Output: return first key which contains the given value
 //    return undefined if no key with given value is found
 
+//ACTUAL FUNCTION
+const findKeyByValue = (object, value) => {
+  let result;
+  // Loops through object use for...in loop
+  for (let key in object) {
+    if (object[key] === value) {
+      result = key;
+    }
+  }
+  return result;
+};
 
 // TEST CASES
 const bestTVShowsByGenre = {
@@ -21,5 +32,17 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
+console.log(findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine'));
+
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const fruitsByColour = {
+  orange: 'orange',
+  yellow: 'banana',
+  red: 'apple'
+};
+
+assertEqual(findKeyByValue(fruitsByColour, 'banana'), 'yellow');
+assertEqual(findKeyByValue(fruitsByColour, 'apple'), 'red');
+assertEqual(findKeyByValue(fruitsByColour, 'kiwi'), undefined);
