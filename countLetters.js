dbc838d69d385c72b764ea98591ba0346db9c0bc
Sandra To <sandra.to.e@gmail.com>
2021-02-04 01:49:52 +0000

    Test and assertion cases added

--- countLetters.js
+++ countLetters.js
@@ -19,6 +19,25 @@ const assertEqual = function(actual, expected) {
 //  Add character of string as object key
 //  Counter increases if condition is true
 //  Default is +1 in order to make the key
+//  Ignore if space
 // End Loop
 // Return object
 // End Function
+
+// TEST CASES
+console.log(countLetters('LHL'));
+console.log(countLetters('hello world'));
+
+assertEqual(countLetters('LHL'), { L: 2, H: 1});
+assertEqual(countLetters('lighthouse labs'), {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+});
