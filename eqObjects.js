718930c31aa9ffa565bb633b30139ee29f53c2a4
Sandra To <sandra.to.e@gmail.com>
2021-02-04 05:23:51 +0000

    findKeyByValue function removed because unrelated

--- eqObjects.js
+++ eqObjects.js
@@ -11,17 +11,6 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 
-// findKeyByValue FUNCTION
-const findKeyByValue = (object, value) => {
-  let result;
-  // Loops through object use for...in loop
-  for (let key in object) {
-    if (object[key] === value) {
-      result = key;
-    }
-  }
-  return result;
-};
 
 // ACTUAL eqObjects FUNCTION
 const eqObjects = function(object1, object2) {
@@ -31,15 +20,15 @@ const eqObjects = function(object1, object2) {
   if (key1Arr.length !== key2Arr.length) {
     return false;
   }
-  // // loop through object1
-  // for (let key1 in object1) {
-  //   if (object1[])
-  // }
-  //   // loop through object2
-  // for (let key2 in object2) {
-  //   // if key-value pairs are different, return false
-  //   if (object2[key2])
-  // }
+  // loop through object1
+  for (let key1 in object1) {
+    if (object1[])
+  }
+    // loop through object2
+  for (let key2 in object2) {
+    // if key-value pairs are different, return false
+    if (object2[key2])
+  }
   
 };
 
