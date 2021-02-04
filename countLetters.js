8a985fb22d2ead08614193d2ba38afa5a874b776
Sandra To <sandra.to.e@gmail.com>
2021-02-04 01:43:47 +0000

    Pseudocode added

--- countLetters.js
+++ countLetters.js
@@ -6,3 +6,19 @@ const assertEqual = function(actual, expected) {
     console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+// CHALLENGE && FUNCTION BEHAVIOUR
+// Create function called countLetters
+// Input: a string
+// Output: return object with a count of each othe letters in that string
+
+// PSEUDOCODE
+// Function expression starts
+// Declare variable empty object
+// Loop through string parameter
+//  Add character of string as object key
+//  Counter increases if condition is true
+//  Default is +1 in order to make the key
+// End Loop
+// Return object
+// End Function
