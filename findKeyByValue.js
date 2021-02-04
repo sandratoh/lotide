2c52b757d33786a1146a3ab5b719cd01c2ff2eec
Sandra To <sandra.to.e@gmail.com>
2021-02-04 04:21:05 +0000

    Pseudocode and Test cases added

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,3 +6,20 @@ const assertEqual = function(actual, expected) {
     console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+// CHALLENGE
+// Function findKeyByValue
+// Input: takes in an object and a value
+// Output: return first key which contains the given value
+//    return undefined if no key with given value is found
+
+
+// TEST CASES
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
