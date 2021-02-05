1c0d5f1f265d8ba9255d480628daabf025ee73e2
Sandra To <sandra.to.e@gmail.com>
2021-02-05 03:09:54 +0000

    Function created and passed all assertion tests

--- findKey.js
+++ findKey.js
@@ -12,6 +12,20 @@ const assertEqual = function(actual, expected) {
 // Output: return the first key for which the callback is truthy
 //  return undefined if no key is found
 
+// ACTUAL FUNCTION
+const findKey = (object, callback) => {
+  let result;
+  for (let key in object) {
+    // console.log(key);
+    // if callback is truthy, return the key
+    if (callback(object[key])) {
+      // console.log(key);
+      return result = key;
+    //   console.log(result);
+    }
+  }
+  return result;
+};
 
 // TEST CODES
 let result1 = findKey({
@@ -24,3 +38,25 @@ let result1 = findKey({
 }, x => x.stars === 2); // => "noma"
 
 assertEqual(result1, 'noma');
+
+let result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3); // => "noma"
+
+assertEqual(result2, 'Akaleri');
+
+let result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5); // => "noma"
+
+assertEqual(result3, undefined);
