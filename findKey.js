3c174500464fdb737915b13eb2fea2afa1cc3fd1
Sandra To <sandra.to.e@gmail.com>
2021-02-05 02:46:46 +0000

    Assertion function and test codes added

---
+++ findKey.js
@@ -0,0 +1,26 @@
+// ASSERTION FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// CHALLENGE
+// Input: an object  and callback
+// Output: return the first key for which the callback is truthy
+//  return undefined if no key is found
+
+
+// TEST CODES
+let result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(result1, 'noma');
