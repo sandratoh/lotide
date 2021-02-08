efb9f952f52d638f4b90159f63545baa693ec648
Sandra To <sandra.to.e@gmail.com>
2021-02-08 23:44:35 +0000

    Refactored and modulized function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,20 +1,5 @@
 // eqObjects Function:
-const eqObjects = function(object1, object2) {
-  let key1Arr = Object.keys(object1);
-  let key2Arr = Object.keys(object2);
-  if (key1Arr.length !== key2Arr.length) {
-    return false;
-  }
-  for (let key of key1Arr) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      return eqArrays(object1[key], object2[key]);
-    }
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects');
 
 // Function behaviour:
 // Input: take in two objects
@@ -31,18 +16,5 @@ const assertObjectsEqual = (actualObj, expectedObj) => {
   }
 };
 
-// Test cases:
-assertObjectsEqual({ a : 1 }, { a : 1});
-assertObjectsEqual({ a : 1 }, { a : '1'});
-
 // Export function:
-module.exports = {
-  assertObjectsEqual: (actualObj, expectedObj) => {
-    const inspect = require('util').inspect;
-    if (eqObjects(actualObj, expectedObj)) {
-      console.log(`✅✅✅ Assertion Passed: ${inspect(actualObj)} === ${inspect(expectedObj)}`);
-    } else {
-      console.log(`⛔️⛔️⛔️ Assertion Failed: ${inspect(actualObj)} !== ${inspect(expectedObj)}`);
-    }
-  }
-};
+module.exports = assertObjectsEqual;
