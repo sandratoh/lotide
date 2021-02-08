a1a9073fc3c9e0c369cd0bee212e3f8f3fdffef2
Sandra To <sandra.to.e@gmail.com>
2021-02-08 22:59:13 +0000

    Export all asertion functions in index

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 
+const assertEqual = require('./assertEqual');
+
+const eqArrays          = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
+const eqObjects          = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
+
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+
+  assertEqual: assertEqual,
+
+  eqArrays:          eqArrays,
+  assertArraysEqual: assertArraysEqual,
+  
+  eqObjects:          eqObjects,
+  assertObjectsEqual: assertObjectsEqual,
 };
