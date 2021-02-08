d347bc08d46af66e5d81500ed1336f6504ae810f
Sandra To <sandra.to.e@gmail.com>
2021-02-08 22:16:09 +0000

    refactor tail function to one line

--- tail.js
+++ tail.js
@@ -4,11 +4,14 @@
 //TAIL FUNCTION RETURNS A NEW ARRAY WITH EVERYTHING EXCEPT FOR THE FIRST ITEM (HEAD) OF THE ORIGINAL ARRAY
 //TAIL FUNCTION SHOULD NOT MODIFY THE ORIGINAL ARRAY
 const tail = arr => {
-  let tailArr = [];
-  for (let i = 1; i < arr.length; i++) {
-    tailArr.push(arr[i]);
-  }
-  return tailArr;
+  // long version
+  // let tailArr = [];
+  // for (let i = 1; i < arr.length; i++) {
+  //   tailArr.push(arr[i]);
+  // }
+  // return tailArr;
+  // simplified one line vr
+  return arr.slice(1);
 };
 
 module.exports = tail;
