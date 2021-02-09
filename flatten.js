187699872bfb761cd009387363b99cfcd65c9eea
Sandra To <sandra.to.e@gmail.com>
2021-02-09 06:43:33 +0000

    Refactor and modulize flatten function, recursion method function added, and test created with mocha and chai

--- flatten.js
+++ flatten.js
@@ -4,20 +4,35 @@
 // assertion function for arrays
 // const assertArraysEqual = require('./assertArraysEqual');
 
-// Iteration method
+// Recursion method
 const flatten = (arr) => {
   let newArr = [];
-  for (let elem of arr) {
-    if (Array.isArray(elem)) {
-      for (let nestElem of elem) {
-        newArr.push(nestElem);
-      }
+  arr.forEach((item) => {
+  // recursive case: if item is in another array
+    if (Array.isArray(item)) {
+      newArr.push(...flatten(item));
+      // newArr.push(item);
     } else {
-      newArr.push(elem);
+      // base case: if item is an element of first array, just print
+      newArr.push(item);
     }
-  }
+  });
   return newArr;
 };
 
+// Iteration method
+// const flatten = (arr) => {
+//   let newArr = [];
+//   for (let elem of arr) {
+//     if (Array.isArray(elem)) {
+//       for (let nestElem of elem) {
+//         newArr.push(nestElem);
+//       }
+//     } else {
+//       newArr.push(elem);
+//     }
+//   }
+//   return newArr;
+// };
 
 module.exports = flatten;
